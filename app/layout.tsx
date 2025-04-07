4625b186a781b265a58b61e491c17390af756e0a
Mistry Pavan Kumar <pavansharma.m0114si@gmail.com>
2025-04-07 02:31:21 -0400

    added skill section

--- app/layout.tsx
+++ app/layout.tsx
@@ -7,7 +7,7 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className="bg-primary">{children}</body>
+      <body className="bg-primary font-lato">{children}</body>
     </html>
   );
 }
